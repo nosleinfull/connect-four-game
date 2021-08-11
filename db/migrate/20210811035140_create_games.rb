72842cb2cd08f43df4c4524a2034384057881b3b
nielson <nielson_fernandes@hotmail.com>
2021-08-11 00:07:25 -0400

    Players can open only one game session each time...

--- db/migrate/20210811035140_create_games.rb
+++ db/migrate/20210811035140_create_games.rb
@@ -1,8 +1,8 @@
 class CreateGames < ActiveRecord::Migration[6.1]
   def change
     create_table :games do |t|
-      t.integer :player_one
-      t.integer :player_two
+      t.integer :player_one, null: false, index: { unique: true }
+      t.integer :player_two, null: false, index: { unique: true }
       t.jsonb :session_data, default: {}, null: false, using: :gin
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -16,11 +16,13 @@ ActiveRecord::Schema.define(version: 2021_08_11_035140) do
   enable_extension "plpgsql"
 
   create_table "games", force: :cascade do |t|
-    t.integer "player_one"
-    t.integer "player_two"
+    t.integer "player_one", null: false
+    t.integer "player_two", null: false
     t.jsonb "session_data", default: {}, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["player_one"], name: "index_games_on_player_one", unique: true
+    t.index ["player_two"], name: "index_games_on_player_two", unique: true
   end
 
 end
