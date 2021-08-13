697cd25de0c0c750d734918cfd2ecc94aa88ab31
nielson <nielson_fernandes@hotmail.com>
2021-08-13 18:09:25 -0400

    Fix debt in game_session_channel.js

--- app/javascript/channels/game_session_channel.js
+++ app/javascript/channels/game_session_channel.js
@@ -1,22 +1,6 @@
 import consumer from "./consumer"
 
-// TODO: Call update board event function from cf_game. Game Session Channel should not know DOM from cf_game views VIOLATION OF DESIGN HERE
-function bindClickSquareEvent(){
-  $(document).find(".square").on('click', function(){
-    var column = $(this).attr('data-col');
-    var player_id = $(this).attr('data-player-id');
-    var url = $(this).attr('data-player-moves-url');
-
-    $.post(url, {
-      authenticity_token: $(document).find('meta[name="csrf-token"]').attr('content'),
-      player_id: player_id,
-      player_moves: { column: column }
-    }, function(data, status) {});
-  });
-}
-
 document.addEventListener('turbolinks:load', () => {
-  bindClickSquareEvent();
   const element = document.getElementById('game-session-id');
   const game_session_id = element.getAttribute('data-game-session-id');
   const player_id = element.getAttribute('data-player-id');
@@ -36,7 +20,7 @@ document.addEventListener('turbolinks:load', () => {
       // Called when there's incoming data on the websocket for this channel
       if (data['data'] == 'change_board'){
         console.log('board updated!')
-        $('.main_board_container').load('/game/' + player_id + '/board', bindClickSquareEvent);
+        $('.main_board_container').load('/game/' + player_id + '/board');
       }
       console.log(data)
     }
--- app/javascript/packs/game_js/cf_game.js
+++ app/javascript/packs/game_js/cf_game.js
@@ -1,4 +1,23 @@
 // app/javascript/packs/cf_game.js
 
+function bindClickSquareEvent(){
+  $(document).find(".square").on('click', function(){
+    var column = $(this).attr('data-col');
+    var player_id = $(this).attr('data-player-id');
+    var url = $(this).attr('data-player-moves-url');
+
+    $.post(url, {
+      authenticity_token: $(document).find('meta[name="csrf-token"]').attr('content'),
+      player_id: player_id,
+      player_moves: { column: column }
+    }, function(data, status) {});
+  });
+}
+
 $(document).on('turbolinks:load', function() {
+  bindClickSquareEvent();
+
+  $(document).on("DOMSubtreeModified", ".main_board_container", function () {
+    bindClickSquareEvent();
+  });
 });
