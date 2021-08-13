4933c3f51bb4bcbf37b5faca758fac1856c60e8b
nielson <nielson_fernandes@hotmail.com>
2021-08-12 20:49:48 -0400

    Adds media query and code create_game_session class

---
+++ spec/game_services/cf_game/create_game_session_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'matrix'
+
+RSpec.describe CfGame::CreateGameSession do
+  describe '#call' do
+    subject { described_class.new(player_one_id: 1, player_two_id: 2, game_model: Game).call }
+
+    context 'when create new game session' do
+      it 'creates a game session with board' do
+        expect(
+          subject.session_data['board_matrix']
+        ).to eq(Matrix.build(6, 7) { 0 }.to_a)
+      end
+
+      it 'creates a game session with player one' do
+        expect(
+          subject.player_one_id
+        ).to eq(1)
+      end
+
+      it 'creates a game session with player two' do
+        expect(
+          subject.player_two_id
+        ).to eq(2)
+      end
+    end
+  end
+end
