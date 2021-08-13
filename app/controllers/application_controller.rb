fffd2a119d7b5f89411491bd316e2cf0cea5f317
nielson <nielson_fernandes@hotmail.com>
2021-08-12 23:03:46 -0400

    Create route to board reloading

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  def current_player
+    params[:id]
+  end
+
+  helper_method :current_player
 end
--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -8,10 +8,4 @@ class GameController < ApplicationController
   def set_game
     @game = Game.find_by(player_one_id: current_player) || Game.find_by!(player_two_id: current_player)
   end
-
-  def current_player
-    params[:id]
-  end
-
-  helper_method :current_player
 end
