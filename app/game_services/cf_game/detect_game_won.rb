f9967a9b05fc702f99db547858384a610f9a9dc4
nielson <nielson_fernandes@hotmail.com>
2021-08-14 16:38:49 -0400

    Finish him

--- app/game_services/cf_game/detect_game_won.rb
+++ app/game_services/cf_game/detect_game_won.rb
@@ -3,6 +3,7 @@
 require 'matrix'
 
 module CfGame
+  # TODO: Tech debts here! Needs refactoring and coding of unit tests(drawing)
   class DetectGameWon
     def initialize(game:)
       @game = game
@@ -16,10 +17,12 @@ module CfGame
     private
 
     attr_accessor :game
+    attr_reader :matrix_with_win_draw
 
     def setup_win
       # TODO: We can identify the win inside de matrix (changing color in front-end)
       game.session_data['game_status'] = 'won'
+      game.session_data['board_matrix'] = matrix_with_win_draw
       game.session_data['winner'] = last_move_value == 1 ? 'player_one' : 'player_two'
       game.save
     end
@@ -31,6 +34,10 @@ module CfGame
 
     def matrix
       @matrix ||= game.session_data['board_matrix']
+    end
+
+    def init_matrix_for_win_draw(original_matrix)
+      @matrix_with_win_draw = original_matrix.deep_dup
     end
 
     def column_matrix
@@ -73,8 +80,12 @@ module CfGame
       matched_number = 0
       row, col = find_min_backward_slash
 
+      init_matrix_for_win_draw(matrix)
+
       while row <= 5 && col <= 6
         matrix[row][col] == last_move_value ? matched_number += 1 : matched_number = 0
+
+        matrix_with_win_draw[row][col] = 3 if matched_number >= 1
 
         return true if matched_number == 4 && !last_move_value.zero?
 
@@ -89,8 +100,12 @@ module CfGame
       matched_number = 0
       row, col = find_min_forward_slash
 
+      init_matrix_for_win_draw(matrix)
+
       while row <= 5 && col >= 0
         matrix[row][col] == last_move_value ? matched_number += 1 : matched_number = 0
+
+        matrix_with_win_draw[row][col] = 3 if matched_number >= 1
 
         return true if matched_number == 4 && !last_move_value.zero?
 
@@ -101,21 +116,33 @@ module CfGame
     end
 
     def won_horizontal?
-      return true if won_array?(matrix[last_row])
+      init_matrix_for_win_draw(matrix)
+      result = won_array?(matrix[last_row], last_row)
+
+      return true if result
 
       false
     end
 
     def won_vertical?
-      return true if won_array?(column_matrix[last_col])
+      init_matrix_for_win_draw(column_matrix)
+
+      if won_array?(column_matrix[last_col], last_col)
+        init_matrix_for_win_draw(Matrix[*matrix_with_win_draw].transpose.to_a)
+
+        return true
+      end
 
       false
     end
 
-    def won_array?(array)
+    def won_array?(array, row)
       matched_number = 0
-      array.each do |value|
+      array.each_with_index do |value, index|
         value == last_move_value ? matched_number += 1 : matched_number = 0
+
+        matrix_with_win_draw[row][index] = 3 if matched_number >= 1
+
         return true if matched_number == 4 && !last_move_value.zero?
       end
 
@@ -123,8 +150,7 @@ module CfGame
     end
 
     def won_game?
-      #             O(NxM)          O(N)             O(N)                O(N)   = O(NXM) + O(N)
-      @won_game ||= [won_vertical?, won_horizontal?, won_forward_slash?, won_backward_slash?].any?
+      @won_game ||= won_vertical? || won_horizontal? || won_forward_slash? || won_backward_slash?
     end
 
     def draw_game?
